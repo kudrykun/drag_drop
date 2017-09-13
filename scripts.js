d2b2ab6d1220a32a6554dffd87de438905300f40
kudrykun <kudrykun@gmail.com>
2017-09-13 23:17:09 +0300

    Главное не кликать на форму

--- scripts.js
+++ scripts.js
@@ -22,11 +22,19 @@ $(document).ready(function(){
 		        for(var i = 0; i < droppedFiles.length; i++)
 		        {
 		          	console.log(droppedFiles[i]);
-		          	$("#messages").append(" Добавленный файл "+ droppedFiles[i].name + "</br>");
 		          	var image = new Image();
 		          	image.onload = function () {
-			            $("#droppedImages .row").append('<div class="col-xs-3"></div>');
-			            $("#droppedImages .row div").last().append(this);
+			            $("#droppedImages .row").append(
+			            	'<div class="col-xs-12 col-sm-6 col-md-3">' +
+				            	'<div class="dz-image">' +
+	                  				'<div class="dz-preview-btn">' +
+	                  				'</div>' +
+	                  				'<div class="dz-delete-btn">' +
+	                  				'</div>' +
+	                  			'</div>' +
+	                  		'</div>'
+                  		);
+			            $("#droppedImages > .row > div > .dz-image").last().append(this);
 			            $(this).addClass("img-responsive");
 			        }
 			        image.src = _URL.createObjectURL(droppedFiles[i]);
@@ -43,4 +51,43 @@ $(document).ready(function(){
   	$("#myDropzone").on('dragover', function(ev) {
     	ev.preventDefault();
   	});
+
+  	$("#dzHiddenInput").on('click', function(ev) {
+	    //Отключает дефолтные события, но это точно предотвращает открытие картинки в браузере
+	    ev.preventDefault();
+	    ev.stopPropagation();
+
+	    // итерация через фзагружаемые файлы
+	    if(ev.originalEvent.dataTransfer){
+	    	if(ev.originalEvent.dataTransfer.files.length) {
+		        var droppedFiles = ev.originalEvent.dataTransfer.files;
+		        for(var i = 0; i < droppedFiles.length; i++)
+		        {
+		          	console.log(droppedFiles[i]);
+		          	var image = new Image();
+		          	image.onload = function () {
+			            $("#droppedImages .row").append(
+			            	'<div class="col-xs-12 col-sm-6 col-md-3">' +
+				            	'<div class="dz-image">' +
+	                  				'<div class="dz-preview-btn">' +
+	                  				'</div>' +
+	                  				'<div class="dz-delete-btn">' +
+	                  				'</div>' +
+	                  			'</div>' +
+	                  		'</div>'
+                  		);
+			            $("#droppedImages > .row > div > .dz-image").last().append(this);
+			            $(this).addClass("img-responsive");
+			        }
+			        image.src = _URL.createObjectURL(droppedFiles[i]);
+		          // Upload droppedFiles[i] to server
+		          // $.post(); to upload file to server
+		        }
+	      	}
+	    }
+		$("#myDropzone").addClass("dz-started");
+    	$("#myDropzone").removeClass("highlightDropArea");
+    	return false;
+  	});
 });
+
